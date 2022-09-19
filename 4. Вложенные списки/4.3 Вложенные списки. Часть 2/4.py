b768fe15ea9f4e97def42a38b6db0377f8e0f7af
RAX3R <45417916+vqpv@users.noreply.github.com>
2022-09-19 23:45:22 +0300

    4.3 - 4

---
+++ 4. Вложенные списки/4.3 Вложенные списки. Часть 2/4.py
@@ -0,0 +1,17 @@
+num = int(input())
+
+lst_1 = [1]
+
+print(*lst_1)
+
+if num != 0:
+    for _ in range(num - 1):
+        lst_2 = lst_1.copy()
+        lst_3 = []
+        lst_1.pop(0)
+        lst_2.pop(-1)
+        for i, j in enumerate(lst_1):
+            lst_3.append(j + lst_2[i])
+        lst_3.insert(0, 1)
+        lst_1 = lst_3 + [1]
+        print(*lst_1)
