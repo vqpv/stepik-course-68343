180228ad075132ae93bc43418a194a28c36ced07
RAX3R <45417916+vqpv@users.noreply.github.com>
2022-09-25 22:51:07 +0300

    4.4 - 6

---
+++ 4. Вложенные списки/4.4 Матрицы. Часть 1/6.py
@@ -0,0 +1,12 @@
+matrix = []
+new_lst = []
+
+n = int(input())
+for i in range(n):
+    s = input().split()
+    for i_2, j_2 in enumerate(s):
+        if (i >= i_2 and i <= n - 1 - i_2) or (i <= i_2 and i >= n - 1 - i_2):
+            new_lst.append(int(j_2))
+    matrix.append(s)
+
+print(max(new_lst))
