61e9482bf2783c4dd0e3372db6e92dfe5d3ac629
RAX3R <45417916+vqpv@users.noreply.github.com>
2022-09-29 21:39:51 +0300

    4.5 - 4

---
+++ 4. Вложенные списки/4.5 Матрицы. Часть 2/4.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+matrix = [[int(i) for i in input().split()] for _ in range(n)]
+result = "YES"
+
+for i in range(n):
+    for j in range(n):
+        if matrix[i][j] != matrix[j][i]:
+            result = "NO"
+
+print(result)
