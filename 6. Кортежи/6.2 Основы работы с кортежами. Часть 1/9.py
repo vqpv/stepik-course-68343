4dcc0007d94d1129b5c6c72d782cf3b10ee64afd
RAX3R <45417916+vqpv@users.noreply.github.com>
2022-10-26 22:22:49 +0300

    6.2 - 9

---
+++ 6. Кортежи/6.2 Основы работы с кортежами. Часть 1/9.py
@@ -0,0 +1,4 @@
+countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Spain', 'Cameroon')
+number = countries.count('Spain')
+
+print(number)
