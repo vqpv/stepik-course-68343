a03886f74b430b1057362a366e38f5f22a7fe4c4
RAX3R <45417916+vqpv@users.noreply.github.com>
2022-11-25 23:00:12 +0300

    8.8 - 2

---
+++ 8. Множества/8.8 Генераторы множеств и frozenset/2.py
@@ -0,0 +1,5 @@
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+
+first_symbol = {i.lower()[0] for i in words}
+
+print(*sorted(first_symbol))
