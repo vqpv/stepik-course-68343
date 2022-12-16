dbbce1ab800f5af7ed8617bba63a6570299da85c
RAX3R <45417916+vqpv@users.noreply.github.com>
2022-12-16 22:32:18 +0300

    10.3 - 4

---
+++ 10. Словари/10.3 Методы словарей/4.py
@@ -0,0 +1,8 @@
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+
+result = {}
+
+for word in s.split():
+    result[word] = result.get(word, 0) + 1
+
+print(max(sorted(result), key=result.get))
