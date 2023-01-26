42beb2c13dfeeb79d285f62f14859f078835412e
RAX3R <45417916+vqpv@users.noreply.github.com>
2023-01-26 23:08:54 +0300

    13.2 - 4

---
+++ 13. Модули decimal, fraction и complex/13.2 Модуль fractions/4.py
@@ -0,0 +1,8 @@
+from fractions import Fraction
+
+a, b = input(), input()
+
+print(f'{a} + {b} = {Fraction(a) + Fraction(b)}')
+print(f'{a} - {b} = {Fraction(a) - Fraction(b)}')
+print(f'{a} * {b} = {Fraction(a) * Fraction(b)}')
+print(f'{a} / {b} = {Fraction(a) / Fraction(b)}')
