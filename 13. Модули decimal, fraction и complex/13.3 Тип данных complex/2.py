72b35878d34e349094526ae31550dd2bdc7b05b4
RAX3R <45417916+vqpv@users.noreply.github.com>
2023-01-30 23:23:51 +0300

    13.3 - 2

---
+++ 13. Модули decimal, fraction и complex/13.3 Тип данных complex/2.py
@@ -0,0 +1,10 @@
+numbers = [3 + 4j, 3 + 1j, -7 + 3j, 4 + 8j, -8 + 10j, -3 + 2j, 3 - 2j, -9 + 9j, -1 - 1j, -1 - 10j, -20 + 15j, -21 + 1j, 1j, -3 + 8j, 4 - 6j, 8 + 2j, 2 + 3j]
+
+max_n = numbers[0]
+
+for n in numbers:
+    if abs(n) > abs(max_n):
+        max_n = n
+
+print(max_n)
+print(abs(max_n))
