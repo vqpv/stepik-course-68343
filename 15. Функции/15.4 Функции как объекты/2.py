c106e5ba6b98e1f799ce597357b713b6baad72db
RAX3R <45417916+vqpv@users.noreply.github.com>
2023-02-07 23:51:37 +0300

    15.4 - 2

---
+++ 15. Функции/15.4 Функции как объекты/2.py
@@ -0,0 +1,6 @@
+points = [(-1, 1), (5, 6), (12, 0), (4, 3), (0, 1), (-3, 2), (0, 0), (-1, 3), (2, 0), (3, 0), (-9, 1), (3, 6), (8, 8)]
+
+def compare_by_distance(point):
+    return (point[0] ** 2 + point[1] ** 2) ** 0.5
+
+print(sorted(points, key=compare_by_distance))
