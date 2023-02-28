e0da1490560473d93c4667076e7325b761224b34
RAX3R <45417916+vqpv@users.noreply.github.com>
2023-02-28 22:25:28 +0300

    15.9 - 2

---
+++ 15. Функции/15.9 Встроенные функции any(), all(), zip(), enumerate()/2.py
@@ -0,0 +1,6 @@
+countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
+capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
+population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
+
+for i in zip(capitals, countries, population):
+    print(f'{i[0]} is the capital of {i[1]}, population equal {i[2]} people.')
